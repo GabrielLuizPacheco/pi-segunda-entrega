931f062c620e7be749105b8dcc4830d8c04992d3
Gabriel Luiz <gabrielluizpacheco260@gmail.com>
2024-05-07 21:32:50 -0300

    Início de Home

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,102 +1,58 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer
+      class="row q-py-md bg-white"
+      style="border-top: 1px solid rgba(0, 0, 0, 0.1)"
+    >
+      <div class="col row justify-center">
+        <q-icon
+          name="mdi-calendar"
+          class="cursor-pointer"
+          size="32px"
+          :color="route.path === '/' ? 'primary' : 'grey-6'"
+          @click="onLogout()"
+        />
+      </div>
+      <div class="col row justify-center">
+        <q-icon
+          name="mdi-magnify"
+          class="cursor-pointer"
+          size="32px"
+          :color="route.path === '/home' ? 'primary' : 'grey-6'"
+        />
+      </div>
+      <div class="col row justify-center">
+        <q-icon
+          name="mdi-account"
+          class="cursor-pointer"
+          size="32px"
+          :color="route.path === '/profile' ? 'primary' : 'grey-6'"
+        />
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useAuthStore } from 'src/stores/auth-store';
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+const route = useRoute();
+const router = useRouter();
+const { logout } = useAuthStore();
 
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function onLogout() {
+  logout();
+  router.push({ path: '/' });
 }
 </script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: none;
+}
+</style>
